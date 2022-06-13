36836aacf83ca748a998ce31b5a2f08f5a3e0439
segoca <107401531+Segoca@users.noreply.github.com>
2022-06-13 12:53:46 +0200

    Add files via upload

---
+++ js/jssego.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+  var $magic = $(".magic"),
+    magicWHalf = $magic.width() / 2;
+  $(document).on("mousemove", function (e) {
+    $magic.css({ left: e.pageX - magicWHalf, top: e.pageY - magicWHalf });
+  });
+});
